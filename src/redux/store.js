b2273ff7d91601a66fa0ce45c093888586d1d0ee
lexeach <80155340+lexeach@users.noreply.github.com>
2026-07-13 18:38:30 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,21 +4,26 @@ import authReducer from "./slices/authSlice";
 import memberReducer from "./slices/memberSlice";
 import dashboardReducer from "./slices/dashboardSlice";
 import hierarchyReducer from "./slices/hierarchySlice";
+import hierarchyCrudReducer from "./slices/hierarchyCrudSlice";
 import reportReducer from "./slices/reportSlice";
 import documentReducer from "./slices/documentSlice";
 import notificationReducer from "./slices/notificationSlice";
 import settingsReducer from "./slices/settingsSlice";
 import tehsilReducer from "./slices/tehsilSlice";
 
-import hierarchyCrudReducer from "./slices/hierarchyCrudSlice";
-
-
 const store = configureStore({
   reducer: {
     auth: authReducer,
     member: memberReducer,
     dashboard: dashboardReducer,
+
+    // Selection state
     hierarchy: hierarchyReducer,
+
+    // CRUD state
+    hierarchyCrud: hierarchyCrudReducer,
+
+    tehsil: tehsilReducer,
     report: reportReducer,
     document: documentReducer,
     notification: notificationReducer,
